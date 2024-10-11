f4dcb628567d3ad77b930a81b74a2600bea615da
Andrew Lee <qaswerty1@gmail.com>
2024-10-11 11:56:53 -0700

    wrote more of my thought process

--- checkdetection.py
+++ checkdetection.py
@@ -19,9 +19,12 @@ def check(king, queen):
     if (king[0] == queen[0]) or (king[1] == queen[1]):
         return True
 
-    #check diagonals
+    # check diagonals
+    # diagonal movement will always have the lateral and vertical equal to each other, remove negatives with abs 
     elif abs(alphaToNum[king[0]] - alphaToNum[queen[0]]) == abs(int(king[1]) - int(queen[1])):
         return True
+    # ran into issue with abs(king[1] - queen[1]) since it was a string and had typeerror, had to declare as int
+
 
     #else false
     else:
